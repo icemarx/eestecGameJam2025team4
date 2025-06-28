39eec9117b12263f94ce6a23cd3a6d7742cc5a72
Aekai <ak4352@student.uni-lj.si>
2025-06-28 10:39:34 +0200

    Add tower sfx and main music

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -9,6 +9,10 @@ public class Tower : MonoBehaviour
     public Transform projectileSpawnPoint;
 
     public float ringRotationSpeed = 100f; // Adjust the speed of rotation
+    public AudioSource moveSfx;
+
+    public AudioSource[] shoot_ap;
+    int shoot_sfx_ix = 0;
 
 
     public void TakeDamage(int damageTaken)
@@ -37,9 +41,22 @@ public class Tower : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             rotationInput = 1f; // Rotate counter-clockwise
-        } else if (Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             rotationInput = -1f; // Rotate clockwise
+        }
+
+        if (rotationInput != 0f)
+        {
+            if (!moveSfx.isPlaying)
+            {
+                moveSfx.Play();
+            }
+        }
+        else
+        {
+            moveSfx.Pause();
         }
 
         // Apply rotation to the object around the Y-axis
@@ -53,5 +70,8 @@ public class Tower : MonoBehaviour
 
         Vector2 direction = new Vector2(projectileSpawnPoint.position.x, projectileSpawnPoint.position.z) - new Vector2(transform.position.x, transform.position.z);
         projectile.direction = new Vector3(direction.x, 0, direction.y).normalized;
+
+        shoot_ap[shoot_sfx_ix].Play();
+        shoot_sfx_ix = (shoot_sfx_ix + 1) % shoot_ap.Length;
     }
 }
