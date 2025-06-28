c15e238569c2bbc8315a9da0bb801ba943a4126d
Matjaž Ciglič <matjaz.ciglic.jst@gmail.com>
2025-06-28 04:07:02 +0200

    Implemented "spiral" flight pattern

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public abstract class Ship : MonoBehaviour
 {
+    // type
+    public int type = 0;
+    public const int C_FRIENDLY = 0;
+    public const int C_STRAIGHT = 0;
+    public const int C_ZIGZAG = 1;
+    public const int C_SPIRAL = 2;
+
     // movement
     public float speed = 1;
     public Transform target;
+    private float zigzagFrequency = 5f;
+    private float zigzagMagnitude = 0.5f;
+    private float spiralSpeed = 2f;
+    private float spiralRadius = 0.5f;
+    private float timeAlive = 0f;
 
     // health and damage
     public int damage = 10;
@@ -44,8 +56,33 @@ public abstract class Ship : MonoBehaviour
         // switch this.type
 
         // if this.type == straight:
-        Vector3 direction = (target.position - transform.position).normalized;
-        transform.position += direction * speed * Time.fixedDeltaTime;
+        switch (type)
+        {
+            // case C_FRIENDLY:
+            case C_STRAIGHT:
+            Vector3 direction = (target.position - transform.position).normalized;
+            transform.position += direction * speed * Time.fixedDeltaTime;
+            break;
+            case C_ZIGZAG:
+            Debug.Log("Zigzag");
+            direction = (target.position - transform.position).normalized;
+            transform.position += direction * speed * Time.fixedDeltaTime;
+            break;
+            case C_SPIRAL:
+            Debug.Log("Spiral");
+            Vector3 forward = (target.position - transform.position).normalized;
+            Vector3 right = Vector3.Cross(forward, Vector3.up).normalized * 2f;
+            Vector3 offsetTarget = (forward + right);
+            Vector3 prev = transform.position;
+            transform.position += offsetTarget.normalized * speed * Time.fixedDeltaTime;
+
+            transform.LookAt(transform.position + (transform.position - prev));
+            break;
+        }
+            
+        if (type == C_FRIENDLY || type == C_STRAIGHT)
+        {
+        }
 
         // if this.type == "zigzag":
         //transform.position.x += sin(Time.time) * 2 - 1 ;
